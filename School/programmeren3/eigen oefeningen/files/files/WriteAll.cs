e2414c15126470bacbf5a6a26b72a2590d469126
jeffrey tiberghien <jeffrey.tiberghien@gmail.com>
2020-02-17 09:39:05 +0100

    created write all

---
+++ School/programmeren3/eigen oefeningen/files/files/WriteAll.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+
+namespace files
+{
+    class Writeall
+    {
+        static void Main(string[] args)
+        {
+            string fileName = @"./test/Myfile.txt";
+            try
+            {
+                Console.Write("Specify number of lines to write");
+                int n = Convert.ToInt32(Console.ReadLine());
+                string[] arrayLines = new string[n];
+                Console.Write("specify the {0} strings");
+                for (var i = 0; i < n; i++)
+                {
+                    Console.Write("line {0}: ", i + 1);
+                    arrayLines[i] = Console.ReadLine();
+                }
+            }
+
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(fileName))
+                {
+                    string line = "";
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+
+
+    }
+}
