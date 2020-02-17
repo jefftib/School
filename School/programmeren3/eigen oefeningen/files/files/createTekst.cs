b4bbe7c289bcd0b864baabbe47bf0a2a9c77b20f
jeffrey tiberghien <jeffrey.tiberghien@gmail.com>
2020-02-17 09:22:51 +0100

    rebuilding repo

---
+++ School/programmeren3/eigen oefeningen/files/files/createTekst.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace files
+{
+    class createTekst
+    {
+        static void Main(string[] args)
+        {
+            string fileName = @"./test/Myfile.txt";
+            try
+            {
+                using (StreamWriter fileStream = File.CreateText(fileName))
+                {
+                    fileStream.WriteLine("hello and welcome");
+                }
+            }
+
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(fileName))
+                {
+                    string line = " ";
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+
+
+    }
+}
---
+++ School/programmeren3/eigen oefeningen/files/files/delete.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace files
+{
+    class delete
+    {
+
+        static void Main(string[] args)
+        {
+            string fileName = @"./test/Myfile.txt";
+
+      
+                if (File.Exists(fileName))
+                    File.Delete(fileName);
+            Console.WriteLine($"file:{fileName} is deleted succesfully");
+                }
+           
+        }
+
+        }
