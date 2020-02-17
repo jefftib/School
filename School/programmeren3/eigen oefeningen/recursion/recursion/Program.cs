0db01c000bedeb0c944cba5ef5ee6a429a39c944
jeffrey tiberghien <jeffrey.tiberghien@gmail.com>
2020-02-17 15:07:38 +0100

    recursion'

---
+++ School/programmeren3/eigen oefeningen/recursion/recursion/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace recursion
+{
+    class Program
+    {
+        static int PrintSom(int min, int val)
+        {
+            //stop creterium
+            if (val == min)
+            {
+                return val;
+            }
+            return val + PrintSom(min, val - 1);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Number to print");
+            int val = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("the sum of the first natural nummer is", val, PrintSom(1,val));
+            Console.ReadKey();
+        }
+       
+    }
+}
