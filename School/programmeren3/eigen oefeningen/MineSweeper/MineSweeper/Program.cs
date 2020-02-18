2a147371c27c5a58e5618e2aaa086d4822cf78b0
jeffrey tiberghien <jeffrey.tiberghien@gmail.com>
2020-02-18 14:32:19 +0100

    reupload local repo

---
+++ School/programmeren3/eigen oefeningen/MineSweeper/MineSweeper/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace MineSweeper
+{
+    class Program
+    {
+        #region fields
+
+        const int _mine = -1;
+        static readonly  int _height = Console.WindowHeight;
+        static readonly int _width = Console.WindowWidth;
+        static readonly Random _random = new Random();
+        static readonly float _mineRatio = .1f;
+        static readonly int _mineCount = (int)(_width * _height * _mineRatio);
+        static readonly (int value, bool visible)[,] _board;
+        static readonly (int Column, int Row) _position = (_width / 2, _height / 2);
+        #endregion
+        static void Main(string[] args)
+        {
+            GenerateBoard();
+            RenderBoard();
+            EventLoop();
+        }
+
+        private static void GenerateBoard() 
+        {
+            _board = new (int value, bool Visible)[_width, _height];
+
+            var coordinates = new List<(int Column, int Row)>(); //lijst om te weten waar een mijn staat.
+         
+            // coordinate voor iedere cel 
+            for (int row = 0; row < _height; row++) 
+            {
+
+                for( int column = 0; column <_width; column ++)
+                {
+                    coordinates.Add((column, row));
+
+                }
+            }
+            for (int i = 0; i < _mineCount; i++)
+            {
+                //op deze coordinat word een mijn geplaatst
+                int randomIndex = _random.Next(0, coordinates.Count);
+                (int column, int Row) = coordinates[randomIndex];
+                coordinates.RemoveAt(randomIndex);
+                //Mijn plaatsen en visible false cel onzichtbaar bij opstart
+                _board[column, Row] = (_mine, false);
+            }
+            foreach (var tile in adjacentTiles(column, Row))
+                if (_board[tile.column, tile.row].value != _mine)
+                {
+                    _board[tile.column, tile.row].value++;
+                }
+        }
+
+        private static void RenderBoard()
+        {
+
+        }
+        private static void EventLoop()
+        {
+
+        }
+
+        private static bool IsConsoleResized()
+        {
+            if (Console.WindowHeight != _height || Console.WindowWidth = _width)
+            {
+                Console.Clear();
+                /*Quit.(ExitState.Quit, "Console Resized: MineSweeper is closed.");*/
+                return true;
+            }
+            return false;
+        }
+       
+    }
+}
