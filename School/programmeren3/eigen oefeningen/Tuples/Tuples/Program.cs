ff2555145109fed38b634402fd0afc7028df7674
jeffrey tiberghien <jeffrey.tiberghien@gmail.com>
2020-02-19 11:07:38 +0100

    tuples

---
+++ School/programmeren3/eigen oefeningen/Tuples/Tuples/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Tuples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Tuple.Create(1, 2, 3, 4, 5, 6, 7, Tuple.Create(8, 9, 10, 11, 12, 13));
+            var a =  numbers.Item1;
+           var b = numbers.Item7;
+           var c =  numbers.Rest.Item1;
+            var d = numbers.Rest.Item1.Item1;
+
+        }
+    }
+}
