7de79f34cf2a3d7fa7a59b9ba67696e5d045e21c
jeffrey tiberghien <jeffrey.tiberghien@gmail.com>
2020-02-21 10:36:53 +0100

    Gemeente class

---
+++ School/programmeren3/eigen oefeningen/Straten/Straten/Gemeente.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Straten
+{
+    class Gemeente
+    {
+        public int Id;
+        public string Naam;
+        public Regio Regio;
+    }
+}
