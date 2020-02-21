c9ce5d15c068d23949d1f44b24833cde7e623b97
jeffrey tiberghien <jeffrey.tiberghien@gmail.com>
2020-02-21 11:53:08 +0100

    added CopyAll.cs

---
+++ School/programmeren3/eigen oefeningen/files/files/CopyAll.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+namespace files
+{
+    class CopyAll
+    {
+      
+        public static void Main()
+        {
+          
+        }
+
+        
+
+    }
+}
