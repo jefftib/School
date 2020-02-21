ec69a39d1c4f82815fcb5739bbe2746ad5d22e82
jeffrey tiberghien <jeffrey.tiberghien@gmail.com>
2020-02-21 12:19:00 +0100

    MineSweeper

--- School/programmeren3/eigen oefeningen/MineSweeper/MineSweeper/Program.cs
+++ School/programmeren3/eigen oefeningen/MineSweeper/MineSweeper/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 
 namespace MineSweeper
@@ -14,16 +15,22 @@ namespace MineSweeper
         static readonly Random _random = new Random();
         static readonly float _mineRatio = .1f;
         static readonly int _mineCount = (int)(_width * _height * _mineRatio);
-        static  (int value, bool visible)[,] _board;
+      
+
+        static (int value, bool visible)[,] _board;
         static  (int Column, int Row) _position = (_width / 2, _height / 2);
         #endregion
         static void Main(string[] args)
         {
             GenerateBoard();
-            RenderBoard();
-            EventLoop();
+          /*  RenderBoard();
+            EventLoop();*/
         }
+        private static IEnumerable<(int Column, int row )> AdjacentTiles(int column, int row)
+        {
+           
 
+        }
         private static void GenerateBoard() 
         {
             _board = new (int value, bool Visible)[_width, _height];
@@ -37,7 +44,6 @@ namespace MineSweeper
                 for( int column = 0; column <_width; column ++)
                 {
                     coordinates.Add((column, row));
-
                 }
             }
             for (int i = 0; i < _mineCount; i++)
@@ -49,48 +55,15 @@ namespace MineSweeper
                 //Mijn plaatsen en visible false cel onzichtbaar bij opstart
                 _board[column, Row] = (_mine, false);
             }
-            foreach (var tile in adjacentTiles(column, Row))
+
+           
+           
+
+            foreach (var tile in AdjacentTiles(column,Row))
                 if (_board[tile.column, tile.row].value != _mine)
                 {
                     _board[tile.column, tile.row].value++;
 
-                    switch (_board[tile.column, tile.row].value)
-                    {
-                        case -1:
-                            Console.WriteLine("@");
-                            break;
-                        case 0:
-                            Console.WriteLine(" ");
-                            break;
-                        case 1:
-                            Console.WriteLine("1");
-                            break;
-                        case 2:
-                            Console.WriteLine("2");
-                            break;
-                        case 3:
-                            Console.WriteLine("3");
-                            break;
-                        case 4:
-                            Console.WriteLine("4");
-                            break;
-                        case 5:
-                            Console.WriteLine("5");
-                            break;
-                        case 6:
-                            Console.WriteLine("6");
-                            break;
-                        case 7:
-                            Console.WriteLine("7");
-                            break;
-                        case 8:
-                            Console.WriteLine("8");
-                            break;
-                        case 9:
-                            Console.WriteLine("9");
-                            break;
-
-                    }
                 }
         }
 
